bb5a7169ea3fdd398a0ab65a0576b85fa1518420
sairaj chikyala <sairaj.chikyala@gmail.com>
2024-06-12 15:13:52 +0530

    variables demo

--- 03-variables/vars.tf
+++ 03-variables/vars.tf
@@ -9,8 +9,27 @@ variable "x" {
 
 variable "y" {}
 
+variable "y_list" {
+  default = [1,2,3,4]
+}
+
+variable "z_map" {
+  default = {
+    x = 10
+    y = 20
+  }
+}
+
 # print variables
 
 output "x" {
   value = "var.x"
+}
+
+output "y" {
+  value = "var.y_list"
+}
+
+output "z" {
+  value = "var.z_map"
 }
