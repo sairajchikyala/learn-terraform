23be48636ba16afffad4c124ed4090ed3f083a49
sairaj chikyala <sairaj.chikyala@gmail.com>
2024-06-12 19:05:15 +0530

    data source demo

--- 05-module/demo/ec2.tf
+++ 05-module/demo/ec2.tf
@@ -1,4 +1,11 @@
 resource "aws_instance" "test" {
   ami           = "ami-031d574cddc5bb371"
   instance_type = "t3.small"
+  tags = {
+    Name = var.Name
+  }
 }
+
+variable "Name" {}
+
+
--- 05-module/main.tf
+++ 05-module/main.tf
@@ -1,8 +1,10 @@
 module "demo-1" {
   source = "./demo"
+  Name = "demo-1"
 }
 
 
 module "demo-2" {
   source = "./demo"
+  Name = "demo-2"
 }
