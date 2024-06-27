e1d1849407c041bec8dc3c1819752810f2f4b16d
sairaj chikyala <sairaj.chikyala@gmail.com>
2024-06-27 15:13:39 +0530

    decoupling of provisioners

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -22,3 +22,33 @@ resource "aws_instance" "test" {
 data "aws_security_group" "selected" {
   name = "allow-all"
 }
+
+# decouple the provisioners inthe following secenarios
+# if provisioners fails the instance then we donot want to recreate my instance
+# requriments emerge in the way that we need to re-run the provisioners again and again(meaning that provison commands we need to re-run again and again)
+
+resource "aws_instance" "test1" {
+  ami                    = "ami-031d574cddc5bb371"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+}
+resource "null_resource" "provisioner" {
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.test1.public_ip
+    }
+    inline = [
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
+}
+
+
+
+
+
